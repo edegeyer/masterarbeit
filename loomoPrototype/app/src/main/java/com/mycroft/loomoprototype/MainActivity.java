3adff88e197eaf0500a100608c32b3f9696596dd
Martin <meisoldt@googlemail.com>
2019-07-22 14:14:09 +0200

    added loomo app with buttons for tuning

---
+++ loomoPrototype/app/src/main/java/com/mycroft/loomoprototype/MainActivity.java
@@ -0,0 +1,67 @@
+package com.mycroft.loomoprototype;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.segway.robot.sdk.base.bind.ServiceBinder;
+import com.segway.robot.sdk.locomotion.sbv.Base;
+
+import java.util.Timer;
+
+public class MainActivity extends AppCompatActivity {
+
+    Base mBase;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setUpLoomo();
+        final Button turnLeftButton = findViewById(R.id.turnLeftButton);
+        turnLeftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                turn("left");
+            }
+        });
+
+    }
+
+    public void setUpLoomo(){
+        mBase=Base.getInstance();
+        mBase.bindService(this.getBaseContext(), new ServiceBinder.BindStateListener() {
+            @Override
+            public void onBind() {
+
+            }
+
+            @Override
+            public void onUnbind(String reason) {
+
+            }
+        });
+    }
+
+    public void turn(String direction){
+        Timer mTimer = new Timer();
+        mBase.setControlMode(Base.CONTROL_MODE_RAW);
+        if (direction.equals("left")){
+            new Thread(){
+                @Override
+                public void run(){
+                    mBase.setAngularVelocity((float)2.2);
+                try {
+                    // sets for how long the robot is turning with that speed
+                    Thread.sleep(1000);
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                // make the robot stop
+                mBase.setAngularVelocity(0);
+                }
+            }.start();
+        }
+    }
+}
