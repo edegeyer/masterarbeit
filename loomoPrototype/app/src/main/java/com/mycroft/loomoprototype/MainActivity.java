066439bea7983175775284deb0d542eee42bb01d
Martin <meisoldt@googlemail.com>
2019-07-22 14:58:25 +0200

    head turning same speed as base

--- loomoPrototype/app/src/main/java/com/mycroft/loomoprototype/MainActivity.java
+++ loomoPrototype/app/src/main/java/com/mycroft/loomoprototype/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.segway.robot.sdk.base.bind.ServiceBinder;
+import com.segway.robot.sdk.locomotion.head.Head;
 import com.segway.robot.sdk.locomotion.sbv.Base;
 
 import java.util.Timer;
@@ -14,6 +15,7 @@ import java.util.Timer;
 public class MainActivity extends AppCompatActivity {
 
     Base mBase;
+    Head mHead;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         final Button checkAudioButton = findViewById(R.id.checkAudioButton);
         final Button simulatePersonDetectedButton = findViewById(R.id.detectedpersonButton);
 
+        // Head ist standardmäßig auf smooth eingestellt, sprich der Kopf wird entsprechen mitbewegt, aber leicht verzögert
+        // TODO: bewegen des Kopfes mit der gleichen Geschwindigkeit wie den restlichen Körper
         turnLeftButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,12 +48,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        checkAudioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: zugriff auf die Mikrofone und ausgeben der Richtung, aus der die Geräusche kommen
+                // TODO: optional: loomo dreht sich entsprechend in die Richtung der Geräusche
+            }
+        });
+
+        simulatePersonDetectedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: Zugriff auf die Kamera und Erkennung von Personen durchführen
+                //TODO: wenn Person erkannt: links&rechts drehen zum signalisieren
+
+                //TODO: wenn Mycroft eingebunden: Ansprechen der Person
+            }
+        });
+
 
     }
 
     public void setUpLoomo(){
+        // setup the base
         mBase=Base.getInstance();
         mBase.bindService(this.getBaseContext(), new ServiceBinder.BindStateListener() {
+            @Override
+            public void onBind() {
+
+            }
+
+            @Override
+            public void onUnbind(String reason) {
+
+            }
+        });
+        // setup the head
+        mHead = Head.getInstance();
+        mHead.bindService(this.getApplicationContext(), new ServiceBinder.BindStateListener() {
             @Override
             public void onBind() {
 
@@ -84,7 +120,10 @@ public class MainActivity extends AppCompatActivity {
         new Thread(){
             @Override
             public void run(){
+                System.out.println(mHead.getHeadJointYaw());
+                mHead.setMode(Head.MODE_ORIENTATION_LOCK);
                 mBase.setAngularVelocity(velocity);
+                mHead.setYawAngularVelocity(velocity);
                 try {
                     // sets for how long the robot is turning with that speed
                     Thread.sleep(1000);
@@ -93,6 +132,8 @@ public class MainActivity extends AppCompatActivity {
                 }
                 // make the robot stop
                 mBase.setAngularVelocity(0);
+                mHead.setYawAngularVelocity(0);
+                mHead.setMode(Head.MODE_SMOOTH_TACKING);
             }
         }.start();
     }
