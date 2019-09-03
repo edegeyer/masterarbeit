d3111ea3037e76530de730cb1e0d2bc973fb4e67
Martin <meisoldt@googlemail.com>
2019-09-03 13:28:38 +0200

    byte data needed to be changed to string

--- mycroft-core-dev/mycroft/fileOpener/__init__.py
+++ mycroft-core-dev/mycroft/fileOpener/__init__.py
@@ -4,20 +4,25 @@ class fileOpener():
     def __init__(self):
         self.fileLocation = '/home/martin/Desktop/masterarbeit/sampleAudio/turnLeft.wav'
 
-    def openFile(self):#, uri, bus):
-        chunk = 512
+    def openFile(self, uri, bus):
+        chunk = 128
         f = open(self.fileLocation, "rb")
+        bus.emit(Message(
+            "AUDIO",
+            {'action': uri}))
         while True:
-            piece = myfile = f.read(chunk)
+            piece  = f.read(chunk)
             print(piece)
+            stringPiece = str(piece)
+            bus.emit(Message(
+                "AUDIO",
+                {'action': stringPiece}))
             if piece == b'':
                 print("end of file")
                 break
 
         #print(f.read())
-        #bus.emit(Message(
-        #    "AUDIO",
-        #    {'action': uri}))
+
         #bus.emit(Message )
         #print("uri is: ", uri)
         # TODO: einfaches emit auf bus
