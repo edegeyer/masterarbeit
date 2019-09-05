55cc1104988f89706f7d84eebb5a6d1cd968b5bc
Martin <meisoldt@googlemail.com>
2019-09-05 10:50:15 +0200

    added audio file

---
+++ mycroft-core-dev/mycroft/fileOpener/__init__.py
@@ -0,0 +1,28 @@
+from mycroft.messagebus.message import Message
+
+class fileOpener():
+    def __init__(self):
+        self.fileLocation = '/home/martin/Desktop/masterarbeit/sampleAudio/booting.wav'
+
+    def openFile(self):#, uri, bus):
+        chunk = 2048
+        f = open(self.fileLocation, "rb")
+        '''while True:
+            piece = f.read(chunk)
+            print(piece)
+            stringPiece = str(piece)
+            bus.emit(Message(
+                "Audio",
+                {'action': stringPiece}))
+            if piece == b'':
+                bus.emit(Message(
+                    "Audio",
+                    {'action': "end"}))
+                break
+'''
+        print(f.read())
+
+        #bus.emit(Message )
+        #print("uri is: ", uri)
+        # TODO: einfaches emit auf bus
+
